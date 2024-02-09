bab553c81dba04f9fa4ebc144c0806493380d797
refat <khondakar00@gmail.com>
2024-02-09 05:32:38 -0700

    fixed infinite loop bug

--- main.py
+++ main.py
@@ -4,17 +4,15 @@ from scraper import get_title
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
-
 post_text = None
 
 
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hello! Thanks for using our BOT")
     global post_text
+    await update.message.reply_text("Hello! Thanks for using our BOT")
     post_text = get_title()
     if post_text is not None:
         await update.message.reply_text(post_text)
-    await handle_auto_update(update)
 
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -24,20 +22,22 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def custom_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("This is a custom command")
 
-# Responses
+
+async def get_updates(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await handle_auto_update(update)
 
 
 def handle_response(text: str) -> str:
     processed: str = text.lower()
-    if 'Get Update' not in processed:
-        return 'Hey there!We are a automated bot so we can not reply to you'
+    if 'start' not in processed:
+        return 'Hey there! We are an automated bot so we cannot reply to you'
 
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message_type: str = update.message.chat.type
     text: str = update.message.text
 
-    print(f'User ({update.message.chat.id}) in {message_type}:"{text}"')
+    print(f'User ({update.message.chat.id}) in {message_type}: "{text}"')
 
     if message_type == 'group':
         if BOT_USERNAME in text:
@@ -54,15 +54,13 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def handle_auto_update(update: Update):
     global post_text
-    last_processed_news: str = post_text
-    while True:
-        post_text = get_title()
-        if post_text is not None and post_text != last_processed_news:
-            await update.message.reply_text(post_text)
-            print(post_text)
-            last_processed_news = post_text
+    new_post_text = get_title()
+    if new_post_text is not None and new_post_text != post_text:
+        await update.message.reply_text(new_post_text)
+        print(new_post_text)
+        post_text = new_post_text
 
-        time.sleep(5)
+    time.sleep(5)
 
 
 async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -76,6 +74,7 @@ if __name__ == '__main__':
     app.add_handler(CommandHandler('start', start_command))
     app.add_handler(CommandHandler('help', help_command))
     app.add_handler(CommandHandler('custom', custom_command))
+    app.add_handler(CommandHandler('get_updates', get_updates))
 
     # Messages
     app.add_handler(MessageHandler(filters.TEXT, handle_message))
@@ -83,6 +82,6 @@ if __name__ == '__main__':
     # error
     app.add_error_handler(error)
 
-    # pol
+    # polling
     print("Polling")
     app.run_polling(poll_interval=3)
